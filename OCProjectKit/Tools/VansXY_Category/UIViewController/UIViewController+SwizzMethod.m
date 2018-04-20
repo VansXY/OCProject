b40b9ece074d7bae094898b3a1d516210a9f298d
肖扬 <xy_dtb@163.com>
2018-04-20 18:08:55 +0800

    添加备注

--- OCProjectKit/Tools/VansXY_Category/UIViewController/UIViewController+SwizzMethod.m
+++ OCProjectKit/Tools/VansXY_Category/UIViewController/UIViewController+SwizzMethod.m
@@ -10,6 +10,17 @@
 
 @implementation UIViewController (SwizzMethod)
 
+/*
+ 注意以下三点：
+ 1.load 方法
+    +load方法能够确保在类的初始化时候调用，这能够保证改变应用行为的一致性，而+initialize在执行时并不提供这种保证，实际上，如果没有直接给这个类发送消息，该方法可能都不会调用到。
+ 2.选择器，方法和实现
+    SEL
+    Method
+    selector
+ 3.调用
+    调用替换的类名，他在运行时替换为调用原先被替换的方法
+ */
 + (void)load {
     Class class = [self class];
     
