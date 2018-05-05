e1171a8b7efd699930bba89f4faa9c19303056d0
肖扬 <xy_dtb@163.com>
2018-05-05 10:21:39 +0800

    上传委托设计模式的学习

--- OCProjectKit/Tools/VansXY_Category/UIViewController/UIViewController+SwizzMethod.m
+++ OCProjectKit/Tools/VansXY_Category/UIViewController/UIViewController+SwizzMethod.m
@@ -25,7 +25,7 @@ static void initialize_Array() {
  1.load 和 initialize 区别
     load：
         1.子类的 load 方法在所有父类的 load 方法执行后再执行，分类的 load 方法在子类的 load 方法执行后执行，
-        2.每个分类的 load 方法执行顺序不能确定先后
+        2.每个分类的 load 方法执行顺序不能确定先后（看编译顺序，可以在complie Source里面设置）
         3.load 方法不会继承，所有不用调用父类的 load 方法
     initialize：
         1.initialize 方法在类或它的子类收到的第一条消息之前调用的，包括类方法和实例方法调用
@@ -33,9 +33,12 @@ static void initialize_Array() {
         3.如果类或子类没有给它发送消息，那么它可能永远都不会触发
  
  2.选择器，方法和实现
-    SEL
-    Method
-    selector
+    SEL方法选择器：通过方法名在类的方法列表中找到该方法
+    Method 方法：用自定义的方法交换原来的方法
+ 
+    ？？？（思考）
+    替换过后怎么调用原来的方法？
+    因为 runtime 是运行时编译的，所有调用自定义的方法，会在编译的时候 dia
  3.调用
     调用替换的类名，他在运行时替换为调用原先被替换的方法
  */
@@ -48,7 +51,7 @@ static void initialize_Array() {
     return objc_getAssociatedObject(self, @selector(addProperty));
 }
 
-
+/** 一般load 方法只触发一次，所以不需要在用 dispatch_once GCD去控制*/
 + (void)load {
     Class class = [self class];
     
@@ -62,6 +65,7 @@ static void initialize_Array() {
 }
 
 - (void)xxx_viewWillAppear:(BOOL)animated {
+    /** 在 runtime 中 B 替换 A，当系统执行 A 方法的时候会被 B 替换，如果想保留 A 的方法再添加 B 的方法，那么该怎么做？如果调 再A 方法，就是 A替换成B，B 再调 A，导致循环调用了，正确的是调用 B 方法，看似自己调自己，其实不是，A 替换 B 方法，B 方法再调 B 方法，此时的 B 已经是 A 的方法了，不会发生循环调用的问题 */
     [self xxx_viewWillAppear:animated];
     NSLog(@"%@", _array);
     NSLog(@"%@", NSStringFromClass(self.class));
